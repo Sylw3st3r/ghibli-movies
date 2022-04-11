9c42cffd4038f8f2b12c92fdab120a0aad0089c0
Sylwester Kucia <sylwester.j.kucia@gmail.com>
2022-04-11 23:03:20 +0200

    third commit

--- src/components/AllMovies.js
+++ src/components/AllMovies.js
@@ -1,49 +1,58 @@
 import { useEffect, useState } from "react";
 import Movie from "./Movie";
 import classes from './AllMovies.module.css';
-
-const getMovies = async () => {
-
-    const response = await fetch("https://ghibliapi.herokuapp.com/films?limit=250", {
-        method: 'GET', 
-        mode: 'cors', 
-        cache: 'default',
-        credentials: 'omit', 
-        redirect: 'follow', 
-        referrerPolicy: 'no-referrer',
-        headers : { 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-      });
-
-      if(response.ok){
-          const data = await response.json();
-          return data;
-      } else {
-          throw new Error(`Something went wrong!!! Status: ${response.status}`);
-      }
-}
+import { ClipLoader } from "react-spinners";
 export default function AllMovies(){
     const [moviesData, setMoviesData] = useState([]);
+    const [isLoading,setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
 
     useEffect(()=>{
+        setIsLoading(true);
         async function addData(){
-            try{
-                setMoviesData(await getMovies());
-            } catch(err){
-                console.log(err);
+            const response = await fetch("https://ghibliapi.herokuapp.com/films?limit=250", {
+            method: 'GET', 
+            mode: 'cors', 
+            cache: 'default',
+            credentials: 'omit', 
+            redirect: 'follow', 
+            referrerPolicy: 'no-referrer',
+            headers : { 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+                }
+            });
+            setIsLoading(false);
+            if(response.ok){
+                setMoviesData(await response.json());
+            } else {
+                throw new Error(`Something went wrong!!! Status: ${response.status}`);
             }
         };
-        addData();
+        try{
+            addData();
+        } catch(err){
+            console.log(err)
+            setError(true);
+        }
     },[])
 
-    return (
-        <div className={classes.pageContainer}>
-            <div className={classes.center}>
-                <img class={classes.logo} src="https://www.pngkey.com/png/full/198-1987073_open-studio-ghibli-logo.png" alt="kurwa"></img>
-            </div>
-            <ul className={classes.moviesContainer}>{moviesData.map(movie =><Movie id={movie.id} key={movie.id} title={movie.title} image={movie.image}></Movie>)}</ul>
+    if(error){
+        return <p>Something went wrong!!!</p>
+    }else if(isLoading){
+        return (
+        <div className={classes.spinner}>
+            <ClipLoader size="10vw"></ClipLoader>;
         </div>
-    )
+        )
+    }  else {
+        return (
+            <div className={classes.pageContainer}>
+                <div className={classes.center}>
+                    <img class={classes.logo} src="https://www.pngkey.com/png/full/198-1987073_open-studio-ghibli-logo.png" alt="kurwa"></img>
+                </div>
+                <ul className={classes.moviesContainer}>{moviesData.map(movie =><Movie id={movie.id} key={movie.id} title={movie.title} image={movie.image}></Movie>)}</ul>
+            </div>
+        )
+    }
 }
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,48 +1,53 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { ClipLoader } from "react-spinners";
 import classes from './MovieDetails.module.css';
 
-
-const getMovie = async (id) => {
-    const response = await fetch(`https://ghibliapi.herokuapp.com/films/${id}`, {
-        method: 'GET', 
-        mode: 'cors', 
-        cache: 'default',
-        credentials: 'omit', 
-        redirect: 'follow', 
-        referrerPolicy: 'no-referrer',
-        headers : { 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-      });
-
-      if(response.ok){
-          const data = await response.json();
-          return data;
-      } else {
-          throw new Error(`Something went wrong!!! Status: ${response.status}`);
-      }
-}
 
 export default function MovieDetails(){
     const {movieId} = useParams();
     const [movieDescriptionData, setMovieDescriptionData] = useState(null);
+    const [isLoading,setIsLoading] = useState(false);
 
     useEffect(()=>{
         async function addData(){
-            try{
-                setMovieDescriptionData(await getMovie(movieId));
-            } catch(err){
-                console.log(err);
-            }
+            setIsLoading(true)
+            const response = await fetch(`https://ghibliapi.herokuapp.com/films/${movieId}`, {
+                method: 'GET', 
+                mode: 'cors', 
+                cache: 'default',
+                credentials: 'omit', 
+                redirect: 'follow', 
+                referrerPolicy: 'no-referrer',
+                headers : { 
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+              });
+              if(response.ok){
+                setIsLoading(false);
+                setMovieDescriptionData(await response.json());
+              } else {
+                  setIsLoading(false);
+                  throw new Error(`Something went wrong!!! Status: ${response.status}`);
+              }
         };
-        addData();
+        
+        try{
+            addData();
+        } catch(err) {
+            console.log(err)
+        }
 
-    },[movieId,setMovieDescriptionData])
+    },[movieId])
 
-    
-    if(movieDescriptionData){
+    if(isLoading){
+        return (
+            <div className={classes.spinner}>
+                <ClipLoader size="10vw"></ClipLoader>;
+            </div>
+            )
+    } else if(movieDescriptionData) {
         return (
             <div className={classes.layout}>
                     <div className={classes.description}>
@@ -55,7 +60,5 @@ export default function MovieDetails(){
                     </div>
                 </div>     
         )
-    } else {
-        <div>Kurwa miało wyjść inaczej</div>
     }
 }
